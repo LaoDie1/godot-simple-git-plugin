0fb0324e90956c1d3b1bb1674044c4f94ee74a97
LaoDie1 <1693619798@qq.com>
2024-04-03 01:29:50 +0800

    提交文件

---
+++ addons/git_plugin/src/git/git_add.gd
@@ -0,0 +1,17 @@
+#============================================================
+#    Git Add
+#============================================================
+# - author: zhangxuetu
+# - datetime: 2024-04-03 01:02:55
+# - version: 4.2.1.stable
+#============================================================
+class_name GitPlugin_Add
+
+
+static func execute(params):
+	var command = ["git", "add"]
+	if params is Array or params is PackedStringArray:
+		command.append_array(params)
+	elif params is String:
+		command.append(params)
+	return await GitPlugin_Console.execute(command)
--- addons/git_plugin/src/panel/commit_panel.gd
+++ addons/git_plugin/src/panel/commit_panel.gd
@@ -11,6 +11,8 @@ extends Panel
 @onready var unstaged_changes_file_tree: GitPlugin_FileTree = %UnstagedChangesFileTree
 @onready var staged_changes_file_tree: GitPlugin_FileTree = %StagedChangesFileTree
 @onready var committed_file_tree: GitPlugin_FileTree = %CommittedFileTree
+@onready var commit_message_text_edit: TextEdit = %CommitMessageTextEdit
+@onready var commit_message_prompt_animation_player: AnimationPlayer = %CommitMessagePromptAnimationPlayer
 
 
 #============================================================
@@ -26,6 +28,7 @@ func _ready() -> void:
 #============================================================
 func update_files():
 	var data = await GitPlugin_Status.execute(["-u"])
+	#print( JSON.stringify(data, "\t") )
 	
 	var untracked_files : Array = data.get("Untracked files:", [])
 	var changes_not_staged_for_commit : Array = data.get("Changes not staged for commit:", [])
@@ -54,25 +57,6 @@ func _on_add_all_pressed() -> void:
 	unstaged_changes_file_tree.clear_select_items()
 
 
-func _on_commit_changes_pressed() -> void:
-	var list = []
-	for file in committed_file_tree.get_selected_file():
-		var idx = file.find(":")
-		if idx > -1:
-			file = file.substr(idx + 1)
-		list.append('\"' + file.strip_edges() + '\"')
-	
-	var command = ["git", "add"]
-	command.append_array(list)
-	var result = await GitPlugin_Console.execute(command)
-	
-	print(" ".join(command))
-	print_debug("   添加完成 ", result)
-	
-	update_files()
-	
-
-
 func _on_unstaged_changes_file_tree_action_file(item_file: String) -> void:
 	unstaged_changes_file_tree.remove_item(item_file)
 	staged_changes_file_tree.add_item(item_file)
@@ -94,19 +78,33 @@ func _on_add_staged_files_pressed() -> void:
 	var files = staged_changes_file_tree.get_selected_file()
 	if files.is_empty():
 		return
-	files = Array(files).map(
-		func(item: String):
-			var idx = item.find(":")
-			if idx > -1:
-				return item.substr(idx + 1).strip_edges()
-			return item
-	)
 	
-	# 添加提交的文件
-	var command = [ "git", "add" ]
-	command.append_array(files)
-	GitPlugin_Console.execute(command)
+	# 获取文件列表
+	var list = []
+	for file in committed_file_tree.get_selected_file():
+		var idx = file.find(":")
+		if idx > -1:
+			file = file.substr(idx + 1)
+		list.append('\"' + file.strip_edges() + '\"')
+	
+	var result = await GitPlugin_Add.execute(list)
+	print_debug(result)
 	
 	# 添加到树中
 	committed_file_tree.add_items(staged_changes_file_tree.get_selected_file())
 	staged_changes_file_tree.clear_select_items()
+
+
+func _on_commit_changes_pressed() -> void:
+	if committed_file_tree.get_selected_file().is_empty():
+		return
+	if commit_message_text_edit.text.strip_edges() == "":
+		commit_message_prompt_animation_player.play("flicker")
+		return
+	
+	var result = await GitPlugin_Commit.execute(commit_message_text_edit.text.strip_edges())
+	
+	print_debug("   已提交 ", result)
+	
+	update_files()
+
