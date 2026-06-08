635e14f2395b6b8292db18231b2d6da71b3c622e
热心市民张学徒 <34774872+LaoDie1@users.noreply.github.com>
2026-06-08 15:43:11 +0800

    更新显示的图标

--- addons/git_plugin/src/scene/file_tree.gd
+++ addons/git_plugin/src/scene/file_tree.gd
@@ -9,11 +9,13 @@
 class_name GitPlugin_FileTree
 extends Tree
 
+# item_text: 显示到 TreeItem 中的文本
+# file: 这个 TreeItem 的文件路径
 
-signal actived_file(item_file: String, file: String)
-## item_file: 原始的带有其他信息的文件名
-## file: 纯文件名，没有其他信息
-signal edited_file(item_file: String, file: String)
+signal actived_file(item_text: String, file: String)
+signal edited_file(item_text: String, file: String)
+signal deleted_file(item_text: String, file: String)
+
 
 enum ButtonID {
 	EDIT,
@@ -21,13 +23,14 @@ enum ButtonID {
 	ACTIVE,
 }
 
+@export var enabled_action : bool = true
+@export var enabled_edit : bool = true
+@export var enabled_delete : bool = true
 @export var action_texture : Texture2D
+@export_multiline() var active_button_tooltip: String = ""
 @export var new_file_color : Color = Color8(143, 171, 130)
 @export var modified_file_color : Color = Color8(250, 227, 69)
 @export var deleted_file_color : Color = Color8(196, 89, 89)
-@export var enabled_edit : bool = true
-@export var enabled_delete : bool = true
-@export var enabled_action : bool = true
 
 var _last_select_item : TreeItem
 var _root : TreeItem
@@ -41,14 +44,14 @@ func _init() -> void:
 	
 	_root = create_item()
 	button_clicked.connect(button_click)
-	item_activated.connect(
-		func():
-			# 双击
-			var item = get_selected()
-			var item_file = item.get_meta("item_file")
-			var file = item.get_meta("file")
-			actived_file.emit(item_file, file)
-	)
+	#item_activated.connect(
+		#func():
+			## 双击
+			#var item = get_selected()
+			#var item_text = item.get_meta("item_text")
+			#var file = item.get_meta("file")
+			#actived_file.emit(item_text, file)
+	#)
 	item_selected.connect(
 		func():
 			await Engine.get_main_loop().process_frame
@@ -88,27 +91,27 @@ func init_items(items: Array):
 
 
 func add_items(items: Array):
-	for item_file:String in items:
-		add_item(item_file)
+	for item_text:String in items:
+		add_item(item_text)
 
 
-func add_item(item_file: String):
-	if _file_to_item_dict.has(item_file):
+func add_item(item_text: String):
+	if _file_to_item_dict.has(item_text):
 		return
 	
-	var tag : String = item_file[1] if item_file[1] != " " else item_file[0]
-	var type_desc : String = GitPlugin_Status.get_type_description(item_file)
+	var tag : String = item_text[1] if item_text[1] != " " else item_text[0]
+	var type_desc : String = GitPlugin_Status.get_type_description(item_text)
 	if tag == GitPlugin_Status.Type.untracked:
 		type_desc = "New File"
 	
-	var file : String = item_file.substr(3)
+	var file : String = item_text.substr(3)
 	if tag == GitPlugin_Status.Type.renamed:
-		file = file.split("->")[1].strip_edges(true, false)
+		file = file.split("->", true, 1)[1].strip_edges(true, false)
 	
 	# 创建
 	var item : TreeItem = _root.create_child()
 	item.set_cell_mode(0, TreeItem.CELL_MODE_CHECK)
-	set_checked(item, _file_to_checked_dict.get(item_file, true), item_file)
+	set_checked(item, _file_to_checked_dict.get(item_text, true), item_text)
 	
 	# 颜色
 	match tag:
@@ -131,11 +134,9 @@ func add_item(item_file: String):
 	
 	# 文件名
 	item.set_text(0, file + " (%s)" % type_desc.capitalize())
-	item.set_tooltip_text(0, item_file)
-	if file.begins_with("addons/apprentice/src/json_ut"):
-		pass
+	item.set_tooltip_text(0, item_text)
 	item.set_meta("file", file)
-	item.set_meta("item_file", item_file)
+	item.set_meta("item_text", item_text)
 	
 	# 图标
 	item.set_icon_max_width(0, 16)
@@ -143,20 +144,16 @@ func add_item(item_file: String):
 	
 	# 按钮
 	if enabled_edit and Engine.is_editor_hint():
-		item.add_button(0, GitPlugin_Icons.get_icon("File"), ButtonID.EDIT) # 编辑
-		item.set_button_tooltip_text(0, ButtonID.EDIT, "Edit this file")
+		item.add_button(0, GitPlugin_Icons.get_icon("Edit"), ButtonID.EDIT, false, "Edit this file")
 	if enabled_delete:
-		item.add_button(0, GitPlugin_Icons.get_icon("Close"), ButtonID.DELETE) # 删除
-		item.set_button_tooltip_text(0, ButtonID.EDIT, "Delete this file")
-	if enabled_action:
-		if action_texture:
-			item.add_button(0, action_texture, ButtonID.ACTIVE)
-	
-	_file_to_item_dict[item_file] = item
+		item.add_button(0, GitPlugin_Icons.get_icon("Close"), ButtonID.DELETE, false, "Delete this file")
+	if enabled_action and action_texture:
+		item.add_button(0, action_texture, ButtonID.ACTIVE, false, active_button_tooltip, active_button_tooltip)
+	_file_to_item_dict[item_text] = item
 
 
 func clear_items():
-	for child in _root.get_children():
+	for child: TreeItem in _root.get_children():
 		_root.remove_child(child)
 
 
@@ -165,35 +162,35 @@ func clear_select_items():
 	children.reverse()
 	for child in children:
 		if child.is_checked(0):
-			remove_item(child.get_meta("item_file"))
+			remove_item(child.get_meta("item_text"))
 
 
-func remove_item(item_file: String):
-	if _file_to_item_dict.has(item_file):
-		var item : TreeItem = _file_to_item_dict[item_file]
+func remove_item(item_text: String):
+	if _file_to_item_dict.has(item_text):
+		var item : TreeItem = _file_to_item_dict[item_text]
 		_root.remove_child(item)
-		_file_to_item_dict.erase(item_file)
+		_file_to_item_dict.erase(item_text)
 
 
-func set_checked(item: TreeItem, checked: bool, item_file: String = ""):
-	if item_file == "":
-		item_file = item.get_meta("item_file")
+func set_checked(item: TreeItem, checked: bool, item_text: String = ""):
+	if item_text == "":
+		item_text = item.get_meta("item_text")
 	item.set_checked(0, checked)
-	_file_to_checked_dict[item_file] = checked
+	_file_to_checked_dict[item_text] = checked
 
 
 ## 获取选中的文件原始名
 func get_selected_item_file() -> PackedStringArray:
-	var list = PackedStringArray()
+	var list := PackedStringArray()
 	for item in _root.get_children():
 		if item.is_checked(0):
-			list.append(item.get_meta("item_file"))
+			list.append(item.get_meta("item_text"))
 	return list
 
 
 ## 获取选中的文件路径
 func get_selected_files() -> PackedStringArray:
-	var list = PackedStringArray()
+	var list := PackedStringArray()
 	for item in _root.get_children():
 		if item.is_checked(0):
 			list.append(item.get_meta("file"))
@@ -205,19 +202,18 @@ func get_files():
 	return _file_to_item_dict.keys()
 
 
-
 func button_click(item: TreeItem, column: int, id: int, mouse_button_index: int):
-	
-	var file : String = item.get_meta("file")
-	var item_file : String = item.get_meta("item_file")
-	match id:
-		ButtonID.EDIT:
-			edited_file.emit(item_file, file)
-		ButtonID.DELETE:
-			pass
-			#print("删除 ", file)
-		ButtonID.ACTIVE:
-			actived_file.emit(item_file, file)
-		_:
-			print("点击", id)
+	if mouse_button_index == MOUSE_BUTTON_LEFT:
+		var file : String = item.get_meta("file")
+		var item_text : String = item.get_meta("item_text")
+		match id:
+			ButtonID.EDIT:
+				edited_file.emit(item_text, file)
+			ButtonID.DELETE:
+				deleted_file.emit(item_text, file)
+				#print("删除 ", file)
+			ButtonID.ACTIVE:
+				actived_file.emit(item_text, file)
+			_:
+				print("点击", id)
 	
--- addons/git_plugin/src/util/icons.gd
+++ addons/git_plugin/src/util/icons.gd
@@ -9,11 +9,25 @@ class_name GitPlugin_Icons
 
 const ICON = preload("uid://dmomu7e45rfed")
 
+static var _handle_icon_cache: Dictionary[String, Texture2D] = {}
+
 static func get_icon_by_path(file: String) -> Texture2D:
-	match file.get_extension():
+	var ext = file.get_extension()
+	match ext:
 		"gd": return get_icon("Script")
 		"tscn", "scn": return get_icon("PackedScene")
 		"tres", "res": return get_icon("ResourcePreloader")
+		"png", "jpeg", "jpg", "bmp", "svg":
+			return get_icon("Image")
+		"uid":
+			if not _handle_icon_cache.has(ext):
+				var texture : Texture2D = get_icon("UID")
+				var image : Image = texture.get_image().duplicate()
+				_grayscale(image)
+				_handle_icon_cache[ext] = ImageTexture.create_from_image(image)
+			return _handle_icon_cache[ext]
+		"import":
+			return get_icon("ArrowRight")
 		_:
 			if file.get_file() == "":
 				return get_icon("Folder")
@@ -23,7 +37,37 @@ static func get_icon_by_path(file: String) -> Texture2D:
 static func get_icon(name: String) -> Texture2D:
 	if Engine.is_editor_hint():
 		return EditorInterface.get_base_control().get_theme_icon(name, "EditorIcons")
-	if DisplayServer.is_dark_mode_supported():
-		if not DisplayServer.is_dark_mode():
-			return ICON.get_icon(name, "EditorIcons")
+	#if DisplayServer.is_dark_mode_supported():
+		#if not DisplayServer.is_dark_mode():
+			#return ICON.get_icon(name, "EditorIcons")
 	return ICON.get_icon(name, "EditorIcons")
+
+
+# 让图片灰度化
+static func _grayscale(image: Image) -> void:
+	#interface/theme/base_color
+	if Engine.is_editor_hint():
+		var setting : EditorSettings = EditorInterface.get_editor_settings()
+		var base_color : Color = setting.get_setting("interface/theme/base_color")
+		var invert_color: Color = Color(1.0-base_color.r, 1.0-base_color.g, 1.0-base_color.b, 0.5)
+		var size : Vector2i = image.get_size()
+		var color: Color
+		var v: float
+		for y in size.y:
+			for x in size.x:
+				color = image.get_pixel(x, y)
+				if color.a > 0:
+					v = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b
+					var new_color = Color(v,v,v).blend(invert_color)
+					new_color.a = color.a
+					image.set_pixel(x, y, new_color)
+	else:
+		var size : Vector2i = image.get_size()
+		var color: Color
+		var v: float
+		for y in size.y:
+			for x in size.x:
+				color = image.get_pixel(x, y)
+				if color.a > 0:
+					v = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b
+					image.set_pixel(x, y, Color(v,v,v, color.a))
